7d5bd4b63e046f223b81d57bf2e0e05b45706b6d
Oana Berentan <oana.berentan@yahoo.com>
2019-09-21 21:39:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,12 +23,17 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-    DROP TABLE students
+     DROP TABLE students
     SQL
     DB[:conn].execute(sql)
   end
 
   def save
+    sql = <<-SQL
+     INSERT INTO students(name, grade)
+     VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
